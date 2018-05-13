0b63c6967e6e508ab4c36eca02368e93640c0895
Mr_chenjiahui@163.com <0chenjiahui0>
2018-05-13 14:45:10 +0800

    提交测试代码

---
+++ src/renderer/components/LandingPage.vue
@@ -0,0 +1,152 @@
+<template>
+  <div id="wrapper">
+    <img id="logo" src="~@/assets/logo.png" alt="electron-vue">
+    <main>
+      <div class="left-side">
+        <span class="title">
+          Welcome to your new project!
+        </span>
+        <system-information></system-information>
+      </div>
+
+      <div class="right-side">
+        <div class="doc">
+          <div class="title">Getting Started</div>
+          <p>
+            electron-vue comes packed with detailed documentation that covers everything from
+            internal configurations, using the project structure, building your application,
+            and so much more.
+          </p>
+          <button @click="open('https://simulatedgreg.gitbooks.io/electron-vue/content/')">Read the Docs</button><br><br>
+        </div>
+        <div class="doc">
+          <div class="title alt">Example</div>
+          <!-- <button class="alt" @click="open('https://electron.atom.io/docs/')">Electron</button>
+          <button class="alt" @click="open('https://vuejs.org/v2/guide/')">Vue.js</button> -->
+          <button class="alt" @click="login">测试登录</button>
+          <button v-show="isShow" class="alt" @click="getLoginAVUser">获取AV.User.Current</button>
+          <button v-show="isShow" class="alt" @click="getLoginAVLUser">获取AVL.User.Current</button>
+        </div>
+      </div>
+    </main>
+  </div>
+</template>
+
+<script>
+  import AV from 'leancloud-storage'
+  import AVL from 'leancloud-storage/live-query'
+  import SystemInformation from './LandingPage/SystemInformation'
+
+  export default {
+    name: 'landing-page',
+    components: { SystemInformation },
+    data () {
+      return {
+        isShow: false
+      }
+    },
+    methods: {
+      open (link) {
+        this.$electron.shell.openExternal(link)
+      },
+      login () {
+        AV.User.logInWithMobilePhone('13503498930', '6B4308E762489D86FD98151FBB476D2E').then(result => {
+          alert('登录成功')
+          this.isShow = true
+        }).catch(error => {
+          alert(`登录失败${error.message}`)
+        })
+      },
+      getLoginAVUser () {
+        alert(`AV.User.current()=${JSON.stringify(AV.User.current())}`)
+      },
+      getLoginAVLUser () {
+        alert(`AVL.User.current()=${JSON.stringify(AVL.User.current())}`)
+      }
+    }
+  }
+</script>
+
+<style>
+  @import url('https://fonts.googleapis.com/css?family=Source+Sans+Pro');
+
+  * {
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+  }
+
+  body { font-family: 'Source Sans Pro', sans-serif; }
+
+  #wrapper {
+    background:
+      radial-gradient(
+        ellipse at top left,
+        rgba(255, 255, 255, 1) 40%,
+        rgba(229, 229, 229, .9) 100%
+      );
+    height: 100vh;
+    padding: 60px 80px;
+    width: 100vw;
+  }
+
+  #logo {
+    height: auto;
+    margin-bottom: 20px;
+    width: 420px;
+  }
+
+  main {
+    display: flex;
+    justify-content: space-between;
+  }
+
+  main > div { flex-basis: 50%; }
+
+  .left-side {
+    display: flex;
+    flex-direction: column;
+  }
+
+  .welcome {
+    color: #555;
+    font-size: 23px;
+    margin-bottom: 10px;
+  }
+
+  .title {
+    color: #2c3e50;
+    font-size: 20px;
+    font-weight: bold;
+    margin-bottom: 6px;
+  }
+
+  .title.alt {
+    font-size: 18px;
+    margin-bottom: 10px;
+  }
+
+  .doc p {
+    color: black;
+    margin-bottom: 10px;
+  }
+
+  .doc button {
+    font-size: .8em;
+    cursor: pointer;
+    outline: none;
+    padding: 0.75em 2em;
+    border-radius: 2em;
+    display: inline-block;
+    color: #fff;
+    background-color: #4fc08d;
+    transition: all 0.15s ease;
+    box-sizing: border-box;
+    border: 1px solid #4fc08d;
+  }
+
+  .doc button.alt {
+    color: #42b983;
+    background-color: transparent;
+  }
+</style>
---
+++ src/renderer/main.js
@@ -0,0 +1,35 @@
+import Vue from 'vue'
+import axios from 'axios'
+
+import App from './App'
+import router from './router'
+import store from './store'
+
+import AV from 'leancloud-storage'
+import AVL from 'leancloud-storage/live-query'
+
+if (!process.env.IS_WEB) Vue.use(require('vue-electron'))
+Vue.http = Vue.prototype.$http = axios
+Vue.config.productionTip = false
+
+// LeanCloud初始化配置
+const appId = 'vzFy1xF3AKcUfoDver6ttJR0-gzGzoHsz'
+const appKey = '9B62Q1Ic2AA9uYScJcqzEDnT'
+// 初始化LeanCloud存储模块
+AV.init({
+  appId,
+  appKey
+})
+// 初始化LeanCloud LiveQuery模块
+AVL.init({
+  appId,
+  appKey
+})
+
+/* eslint-disable no-new */
+new Vue({
+  components: { App },
+  router,
+  store,
+  template: '<App/>'
+}).$mount('#app')
